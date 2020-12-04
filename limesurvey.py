cfa682a19aa234b7d1ea8b17bf05b56463bbb77b
Emilio Wuerges <wuerges@gmail.com>
2020-12-04 15:10:21 -0300

    json rpc working

--- limesurvey.py
+++ limesurvey.py
@@ -4,41 +4,46 @@ import typer
 import inspect
 
 your_domain = "localhost"
-your_limesurvey_dir = "srv/http/limesurvey/limesurvey"
+your_limesurvey_dir = "limesurvey/limesurvey"
 url = f"http://{your_domain}/{your_limesurvey_dir}/index.php/admin/remotecontrol"
-
 
 def client(url):
     def client_pars(func):
-        signature = inspect.signature(func)
+        def wrap(*params, **kwargs):
+            payload = { 'method': func.__name__, 'params': params, 'id': 1 }
 
-        def wrap(*args, **kwargs):
-
-            params = {}
-            for k,v in signature.parameters.items():
-                p = kwargs.get(k)
-                if p:
-                    params[k] = p
-                else:
-                    params[k] = v.default
-
-            payload = { 'method': func.__name__, 'params': params }
-            return requests.post(url, json=payload).json()
+            return requests.post(url, json=payload).json()['result']
         return wrap
     return client_pars
 
+# function get_session_key(string $username, string $password, string $plugin = 'Authdb')
 @client(url)
-def auth(username, password, plugin='AuthDb'):
+def get_session_key(username, password, plugin='AuthDb'):
     pass
+
+# function release_session_key(string $sSessionKey)
+@client(url)
+def release_session_key(sSessionKey):
+    pass
+
+# add_participants(string $sSessionKey,integer $iSurveyID,array 
+# $aParticipantData,boolean $bCreateToken = true): array
+@client(url)
+def add_participants(sSessionKey, iSurveyID, aParticipantData, bCreateToken):
+    pass
+
+# copy_survey(string $sSessionKey,integer $iSurveyID_org,string $sNewname): array
+@client(url)
+def copy_survey(sSessionKey, iSurveyID_org, sNewname):
+    pass
+
+from jsonrpcclient.clients.http_client import HTTPClient
 
 def main():
 
-    auth(username="kappa", password="pass")
-    typer.echo(url)
-
-
-
-# function get_session_key(string $username, string $password, string $plugin = 'Authdb')
+    k = get_session_key("admin", "admin")
+    typer.echo(k)
+    typer.echo(release_session_key(k))
 
 if __name__ == "__main__":
     typer.run(main)
