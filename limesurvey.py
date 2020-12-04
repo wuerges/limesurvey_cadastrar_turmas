54f5357588838bd359746313d5e344d1fefa1f62
Emilio Wuerges <wuerges@gmail.com>
2020-12-04 19:47:18 -0300

    almost done

--- limesurvey.py
+++ limesurvey.py
@@ -9,71 +9,88 @@ your_domain = "localhost"
 your_limesurvey_dir = "limesurvey/limesurvey"
 url = f"http://{your_domain}/{your_limesurvey_dir}/index.php/admin/remotecontrol"
 
-def client(url):
-    def client_pars(func):
-        def wrap(*params, **kwargs):
-            payload = { 'method': func.__name__, 'params': params, 'id': 1 }
 
-            return requests.post(url, json=payload).json()['result']
-        return wrap
-    return client_pars
+def client(func):
+    def wrap(self, *params, **kwargs):
+        payload = {'method': func.__name__, 'params': params, 'id': 1}
 
-# function get_session_key(string $username, string $password, string $plugin = 'Authdb')
-@client(url)
-def get_session_key(username, password, plugin='AuthDb'):
-    pass
+        return requests.post(self.url, json=payload).json()['result']
+    return wrap
 
-# function release_session_key(string $sSessionKey)
-@client(url)
-def release_session_key(sSessionKey):
-    pass
 
-# add_participants(string $sSessionKey,integer $iSurveyID,array 
-# $aParticipantData,boolean $bCreateToken = true): array
-@client(url)
-def add_participants(sSessionKey, iSurveyID, aParticipantData, bCreateToken):
-    pass
+class LimeClient:
 
-# copy_survey(string $sSessionKey,integer $iSurveyID_org,string $sNewname): array
-@client(url)
-def copy_survey(sSessionKey, iSurveyID_org, sNewname):
-    pass
+    def __init__(self, url):
+        self.url = url
 
-# activate_tokens
-#    /**
-#         * Activate survey participants (RPC function)
-#         *
-#         * Initialise the survey participant table of a survey where new participant tokens may be later added.
-#         *
-#         * @access public
-#         * @param string $sSessionKey Auth credentials
-#         * @param integer $iSurveyID ID of the Survey where a survey participants table will be created for
-#         * @param array $aAttributeFields  An array of integer describing any additional attribute fields
-#         * @return array Status=>OK when successful, otherwise the error description
-#         */
+    # function get_session_key(string $username, string $password, string $plugin = 'Authdb')
 
-@client(url)
-def activate_tokens(sSessionKey, iSurveyID, aAttributeFields):
-    pass
+    @client
+    def get_session_key(self, username, password, plugin='AuthDb'):
+        pass
 
-from jsonrpcclient.clients.http_client import HTTPClient
+    # function release_session_key(string $sSessionKey)
+    @client
+    def release_session_key(self, sSessionKey):
+        pass
+
+    # add_participants(string $sSessionKey,integer $iSurveyID,array
+    # $aParticipantData,boolean $bCreateToken = true): array
+    @client
+    def add_participants(self, sSessionKey, iSurveyID, aParticipantData, bCreateToken):
+        pass
+
+    # copy_survey(string $sSessionKey,integer $iSurveyID_org,string $sNewname): array
+    @client
+    def copy_survey(self, sSessionKey, iSurveyID_org, sNewname):
+        pass
+
+    # activate_tokens
+    #    /**
+    #         * Activate survey participants (RPC function)
+    #         *
+    #         * Initialise the survey participant table of a survey where new participant tokens may be later added.
+    #         *
+    #         * @access public
+    #         * @param string $sSessionKey Auth credentials
+    #         * @param integer $iSurveyID ID of the Survey where a survey participants table will be created for
+    #         * @param array $aAttributeFields  An array of integer describing any additional attribute fields
+    #         * @return array Status=>OK when successful, otherwise the error description
+    #         */
+
+    @client
+    def activate_tokens(self, sSessionKey, iSurveyID, aAttributeFields):
+        pass
+
+# from jsonrpcclient.clients.http_client import HTTPClient
+
 
 def main(dataset: Path, curso: str):
+
+    your_domain = "localhost"
+    your_limesurvey_dir = "limesurvey/limesurvey"
+    url = f"http://{your_domain}/{your_limesurvey_dir}/index.php/admin/remotecontrol"
+
     data = cadastrar.load_students(dataset, curso)
 
-    k = get_session_key("admin", "admin")
+    client = LimeClient(url)
+
+    k = client.get_session_key("admin", "admin")
     typer.echo(k)
 
-    r = copy_survey(k, 683333, "Auto-avaliação do Curso de Ciência da Computação da UFFS - Chapecó (Cópia 2)")
+    r = client.copy_survey(
+        k, 683333, "Auto-avaliação do Curso de Ciência da Computação da UFFS - Chapecó (Cópia 2)")
     typer.echo(r)
-    #{'status': 'OK', 'newsid': 638847}
     newsid = r['newsid']
 
-    typer.echo(activate_tokens(k, newsid, []))
+    typer.echo(client.activate_tokens(k, newsid, []))
 
-    ps = add_participants(k, newsid, [e.limesurvey() for e in data], True)
+    ps = client.add_participants(
+        k, newsid, [e.limesurvey() for e in data], True)
     typer.echo(ps)
 
-    typer.echo(release_session_key(k))
+    typer.echo(client.release_session_key(k))
+
+
 if __name__ == "__main__":
     typer.run(main)
