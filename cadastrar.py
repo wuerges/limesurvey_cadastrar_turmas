50c0733596bad5768341551489ccd99254145562
Emilio Wuerges <wuerges@gmail.com>
2020-12-04 15:57:35 -0300

    agrupando turmas

--- cadastrar.py
+++ cadastrar.py
@@ -25,22 +25,68 @@ class Estudante:
     def disciplina(self):
         return self.line.disciplina
 
-def main(dataset: Path, curso: str):
-    data = pd.read_csv(dataset, sep=";")
+    def limesurvey(self):
+        n = self.line.nome_estudante.strip().split()
+        last = n[-1]
+        first = " ".join(n[:-1])
+
+        return {"email": self.line.email,"lastname":last,"firstname": first}
+
+
+def load_students(path: Path, curso):
+    data = pd.read_csv(path, sep=";")
+    data = filtra_curso(data, curso)
+    data = filtra_ativos(data)
+    students = []
+    for _, l in data.iterrows():
+        students.append(Estudante(l))
+    return students
+
+def load_turmas(path: Path, curso):
+    data = pd.read_csv(path, sep=";")
     data = filtra_curso(data, curso)
     data = filtra_ativos(data)
 
-    typer.echo(f"Columns: {data.columns}")
+    print(data.columns)
+
+    data = data.groupby(['codigo_turma', 'disciplina'])
+
+    return data
+    
+
+
+def main(dataset: Path, curso: str):
+
+    data = load_students(dataset, curso)
+
+    turmas = load_turmas(dataset, curso)
+    typer.echo(f"Turmas: {len(turmas)}")
+
+    for k,v in turmas:
+        g = turmas.get_group(k)
+        disc = g['disciplina']
+        doc = g['docentes']
+        # typer.echo(turmas.get_group(k))
+        typer.echo(disc)
+        typer.echo(doc)
     # typer.echo(data['descricao_turma'].unique())
     # typer.echo(data.describe())
     # typer.echo(data)
 
-    for i, l in data.iterrows():
-        # if i > 10: break
+    for i, e in enumerate(data):
+        if i > 10: break
 
-        e = Estudante(l)
-        typer.echo(f"Est({i}): {e.nome()} ({e.email()}) -> {e.disciplina()} ({e.line.codigo_turma})")
-        break
+        typer.echo(f"Est: {e.nome()} {e.limesurvey()}")
+
+
+    # data.group_by([''])
+
+    # for i, l in data.iterrows():
+    #     # if i > 10: break
+
+    #     e = Estudante(l)
+    #     typer.echo(f"Est({i}): {e.nome()} ({e.email()}) -> {e.disciplina()} ({e.line.codigo_turma})")
+    #     break
 
 
 if __name__ == "__main__":
--- limesurvey.py
+++ limesurvey.py
@@ -2,6 +2,8 @@ import requests
 import json
 import typer
 import inspect
+import cadastrar
+from pathlib import Path
 
 your_domain = "localhost"
 your_limesurvey_dir = "limesurvey/limesurvey"
@@ -37,13 +39,41 @@ def add_participants(sSessionKey, iSurveyID, aParticipantData, bCreateToken):
 def copy_survey(sSessionKey, iSurveyID_org, sNewname):
     pass
 
+# activate_tokens
+#    /**
+#         * Activate survey participants (RPC function)
+#         *
+#         * Initialise the survey participant table of a survey where new participant tokens may be later added.
+#         *
+#         * @access public
+#         * @param string $sSessionKey Auth credentials
+#         * @param integer $iSurveyID ID of the Survey where a survey participants table will be created for
+#         * @param array $aAttributeFields  An array of integer describing any additional attribute fields
+#         * @return array Status=>OK when successful, otherwise the error description
+#         */
+
+@client(url)
+def activate_tokens(sSessionKey, iSurveyID, aAttributeFields):
+    pass
+
 from jsonrpcclient.clients.http_client import HTTPClient
 
-def main():
+def main(dataset: Path, curso: str):
+    data = cadastrar.load_students(dataset, curso)
 
     k = get_session_key("admin", "admin")
     typer.echo(k)
-    typer.echo(release_session_key(k))
 
+    r = copy_survey(k, 683333, "Auto-avaliação do Curso de Ciência da Computação da UFFS - Chapecó (Cópia 2)")
+    typer.echo(r)
+    #{'status': 'OK', 'newsid': 638847}
+    newsid = r['newsid']
+
+    typer.echo(activate_tokens(k, newsid, []))
+
+    ps = add_participants(k, newsid, [e.limesurvey() for e in data], True)
+    typer.echo(ps)
+
+    typer.echo(release_session_key(k))
 if __name__ == "__main__":
     typer.run(main)
